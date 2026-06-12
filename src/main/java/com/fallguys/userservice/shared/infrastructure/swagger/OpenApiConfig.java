245133fc152795d5ba48cfbccdb86214a459762a
yys7517 <yys7517@naver.com>
2026-06-12 17:39:29 +0900

    setting(ERP-000): swagger config 수정 (#0)

--- src/main/java/com/fallguys/userservice/shared/infrastructure/swagger/OpenApiConfig.java
+++ src/main/java/com/fallguys/userservice/shared/infrastructure/swagger/OpenApiConfig.java
@@ -1,9 +1,14 @@
 package com.fallguys.userservice.shared.infrastructure.swagger;
 
+import java.util.List;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,17 +17,27 @@ public class OpenApiConfig {
 
     public static final String BEARER_AUTH = "bearerAuth";
 
+    @Value("${OPENAPI_SERVER_URL:}")
+    private String serverUrl;
+
     @Bean
     OpenAPI userServiceOpenApi() {
-        return new OpenAPI()
+        OpenAPI openApi = new OpenAPI()
                 .info(new Info()
                         .title("UserService API")
                         .version("v1")
                         .description("ERP 사용자 관리 서비스 API"))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
                 .components(new Components()
                         .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
+
+        if (serverUrl != null && !serverUrl.isBlank()) {
+            openApi.servers(List.of(new Server().url(serverUrl)));
+        }
+
+        return openApi;
     }
 }
